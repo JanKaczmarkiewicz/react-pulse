8e00ede24493e92845bcf8795dfff23919f986c3
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-08-28 17:43:48 +0200

    fix: add autocomplete to prettierrc config

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ const App = () => {
     const [state, setState] = useState(0)
 
     return (
-        <button style={{ color: 'red' }} onClick={() => setState((p) => p + 1)}>
+        <div style={{ color: 'red' }} onClick={() => setState((p) => p + 1)}>
             {state}
-        </button>
+        </div>
     )
 }
 
