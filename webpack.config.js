a2d32df989307b85167bb3b7493cd8fd4abad9ca
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-08-28 13:42:19 +0200

    feat: add typescript loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,19 @@ const webpack = require("webpack");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
   plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
     static: "./dist",
     hot: true,
     port: 3000,
   },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
