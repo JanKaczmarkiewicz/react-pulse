3258236eaf921a5bd647001759c14901605be9b4
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-08-28 17:36:35 +0200

    refactor: format all files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 const App = () => {
-  const [state, setState] = useState(0);
+    const [state, setState] = useState(0)
 
-  return (
-    <button style={{ color: "red" }} onClick={() => setState((p) => p + 1)}>
-      {state}
-    </button>
-  );
-};
+    return (
+        <button style={{ color: 'red' }} onClick={() => setState((p) => p + 1)}>
+            {state}
+        </button>
+    )
+}
 
-export default App;
+export default App
