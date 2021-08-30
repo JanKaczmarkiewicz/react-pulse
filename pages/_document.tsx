8bba205e9e5a927896c8fecd4e8428473e90e088
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-08-30 02:27:18 +0200

    fix: eslint error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,8 +9,10 @@ export default class MyDocument extends Document {
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App) => (props) => {
+            const app = <App {...props} />;
+            return sheet.collectStyles(app);
+          },
         });
 
       const initialProps = await Document.getInitialProps(ctx);
