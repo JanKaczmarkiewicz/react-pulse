64db3d1d29250306c38dca28ade7c02ffe3b038c
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-09-01 23:27:22 +0200

    feat: use next mdx remote

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const withMDX = require("@next/mdx")();
-
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  pageExtensions: ["mdx", "tsx"],
 };
 
-module.exports = withMDX(config);
+module.exports = config;
